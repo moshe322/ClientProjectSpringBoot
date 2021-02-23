8441744e42e632513fc47b30cf89348cd73c3c80
Luis Eduardo <75134763+luiseduardobarreto@users.noreply.github.com>
2021-02-23 14:40:11 -0500

    Configuracion para conectar la aplicacion Spring Boot con Google Cloud Platform usando App Engine y SQL en la nube.

--- src/main/java/com/clients/client/controllers/UserController.java
+++ src/main/java/com/clients/client/controllers/UserController.java
@@ -2,6 +2,11 @@ package com.clients.client.controllers;
 
 import com.clients.client.models.UserModel;
 import com.clients.client.services.UserService;
+import com.sun.tools.jconsole.JConsoleContext;
+import lombok.CustomLog;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 /*
 Anotaciones
@@ -24,6 +30,7 @@ Anotaciones
 @Qualifier Si tenemos varios Beans que puedan aplicar, como por ejemplo 2 DataSources usaré el @Qualifier:
  */
 
+@Slf4j
 @RestController // anortación que de de ser empleada en la capa de presentación presentación, para peticiones de tipo REST.
 @RequestMapping("/user")
 public class UserController {
@@ -34,18 +41,21 @@ public class UserController {
     //Obtener lista de usuarios
     @GetMapping()
     public ArrayList<UserModel> getUsers(){
+        log.info("GET the customers that were added from data base 35.202.185.48:3306/springDB");
         return this.userService.getUsers();
     }
 
     //Actualizar usuario
     @PostMapping()
     public UserModel saveUser (@RequestBody UserModel user){
+        log.info("UPDATE the customer to data base 35.202.185.48:3306/springDB");
         return this.userService.saveUser(user);
     }
 
     //Obtner por ID
     @GetMapping( path = "/{id}")
     public Optional<UserModel> getUserById(@PathVariable("id") Long id){
+        log.info("GET the customers by ID from data base 35.202.185.48:3306/springDB");
         return this.userService.getUserById(id);
     }
 
@@ -58,6 +68,7 @@ public class UserController {
     @DeleteMapping ( path = ("/{id}"))
     public String deleteUser (@PathVariable("id") Long id){
         boolean ok = this.userService.deleteUser(id);
+        log.info("se elimino el usuario con id "+ id);
         String result = (ok) ?  "se elimino el usuario con id "+ id : "No se puedo  eliminar el usuario con id "+ id;
         return result;
     }
